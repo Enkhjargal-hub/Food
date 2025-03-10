10840cd61c35f5821941e53f13810bbcf09d7858
Enkhjargal-hub <Joloenja@gmail.com>
2025-03-10 12:26:25 +0800

    j

--- server/src/controllers/models/order.schema.js
+++ server/src/controllers/models/order.schema.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
-const {Schema, model, models} = mongoose;
+const { Schema, model, models } = mongoose;
 
-const foodOrderItemSchema= new Schema(
+const foodOrderItemSchema = new Schema(
     {
-        food: {type: Schema.Types.ObjectId, ref: "foods"},
-        quantity: Number,
+        food: { type: Schema.Types.ObjectId, ref: "foods" },
+        quantity: { type: Number, required: true },
     },
     { _id: false }
 );
 
-const orderSchema = new Schema ({
-    id: {typez: Schema.Types.ObjectId},
-    user: {type:Schema.Types.ObjectId, ref: "Users"},
-    foodOrderItem: {type: [foodOrderItemSchema], required: true },
+const orderSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+    foodOrderItem: { type: [foodOrderItemSchema], required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 export const OrderModel = models.Orders || model("Orders", orderSchema);
